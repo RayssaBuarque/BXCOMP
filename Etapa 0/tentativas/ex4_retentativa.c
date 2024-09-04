447173e90fe18de20165d66d8c5927240ba32b6e
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-09-04 18:37:49 -0300

    Update ex4_retentativa.c

--- Etapa 0/tentativas/ex4_retentativa.c
+++ Etapa 0/tentativas/ex4_retentativa.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cmpfunc (const void * a, const void * b) {
+int cmpfunc (const void * a, const void * b) { //Não lembro para que serve isso mas é requerido para usar o qsort
    return ( *(int*)a - *(int*)b );
 }
 /*Ao invés de guardar os arranjos em vetores diferentes, guardei tudo em um só para apenas ordenar o vetor
-No fim, quando passa as entradas não faz diferença onde cada uma está sendo armazenada*/
+No fim, quando as entradas são passadas "não faz diferença onde cada uma está sendo armazenada"(na minha opinião)*/
 int main(){
     int T, i, j, k, aux, Vetor[16]; 
-    scanf("%d", &T);
+    scanf("%d", &T); //Ler número de tentativas
     while (T>0){
         for(i=0;i<16;i++){
-            scanf("%d", &Vetor[i]);
+            scanf("%d", &Vetor[i]); //Ler todos os elementos
         }
-        qsort(Vetor, 16, sizeof(int), cmpfunc);
+        qsort(Vetor, 16, sizeof(int), cmpfunc); //Ordenar o Vetor
         for(i=0;i<16;i++){
             printf("%d ", Vetor[i]);
         }
