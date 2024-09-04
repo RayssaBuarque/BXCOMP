0029c1eeda496ce26e639a9fc37c5987b28061b6
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-09-04 16:59:34 -0300

    Create ex3_retentativa.c
    Refiz o exercício e consegui terminar

---
+++ Etapa 0/tentativas/ex3_retentativa.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main (){
+    int T, M, N, R, K, i, j, x, y;
+    scanf("%d", &T); //Ler número de testes
+    while(T>0){
+        scanf("%d %d", &M, &N); //Ler dimensões da matriz
+        int Matriz[M][N]; //preencher a matriz com 0
+        for(i=0;i<M;i++){
+            for(j=0;j<N;j++){
+                Matriz[i][j]=0;
+            }
+        }
+        scanf("%d %d", &R, &K); //Ler número de rôbos e de instruções
+        char ordem[K]; //Armazenar as instruções
+        for(i=0;i<R;i++){
+            scanf("%d %d %s", &x, &y, ordem); //Ler o ponto de partida e as instruções
+            for(j=0;j<K;j++){
+                switch (ordem[j]){
+                case 'D':
+                    y++;
+                    break;
+                case 'E':
+                    y--;
+                    break;
+                case 'C':
+                    x--;
+                    break;
+                case 'B':
+                    x++;
+                    break; 
+                default:
+                    break;
+                }
+            }
+            if(Matriz[x][y]==0){
+                Matriz[x][y]=1;
+            } else {
+                printf("Hmm, parece que tivemos um pequeno contratempo robótico.\n");
+                goto a;
+            }
+        }
+        printf("Eureka! Eu sabia que minha programação era impecável.\n");
+        a:
+        T--;
+    }
+return 0;
+}
